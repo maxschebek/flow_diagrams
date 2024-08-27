8214288a6bd457159ea93ce3588a9adb259600de
Maximilian Schebek <m.schebek@fu-berlin.de>
2024-08-27 17:42:19 +0200

    add packages to set.py to set

--- setup.py
+++ setup.py
@@ -3,5 +3,15 @@ setup(
         name='flows_diagrams',
         python_requires=">=3.10",
         version='0.0',
-        packages=find_packages()
+        packages=find_packages(),
+        install_requires=[
+   'jaxlib==0.4.23',
+   'jax==0.4.23',
+   'jax-md==0.2.8',
+   'equinox==0.11.4',
+   'matplotlib==3.7.0',
+   'numpy==1.25.2',
+   'mdtraj==1.10.0',
+   'scipy==1.9.3',
+]
 )
