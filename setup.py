0d11c4e3af7df4b13aa60eea6b273bb693d26ab6
Maximilian Schebek <m.schebek@fu-berlin.de>
2024-08-28 11:14:18 +0200

    fix typos, add init for train

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-        name='flows_diagrams',
+        name='flow_diagrams',
         python_requires=">=3.10",
         version='0.0',
         packages=find_packages(),
